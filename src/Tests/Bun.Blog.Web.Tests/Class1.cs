21cf5e4973a9633a5c0982353f1622545d440608
huhubun <nongzc@outlook.com>
2017-04-04 19:00:26 +0800

    #2 Use .msbuild file to build MVC and Web API project, and add unit test support

---
+++ src/Tests/Bun.Blog.Web.Tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bun.Blog.Web.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void When_value_is_true_return_true()
+        {
+            // Arrange
+            var value = true;
+
+            // Act
+
+            // Assert
+            Assert.True(value);
+        }
+    }
+}
