19879d049afefaa47faf72c12139d01fa1eddaf4
huhubun <nongzc@outlook.com>
2017-05-12 22:53:54 +0800

    #16 Add category definition domain model

---
+++ src/Libraries/Bun.Blog.Core/Domain/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using Bun.Blog.Core.Data;
+
+namespace Bun.Blog.Core.Domain.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
