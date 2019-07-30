a89cde388664dcf6556c4c5fe5968a106b96a796
huhubun <nongzc@outlook.com>
2019-07-30 23:05:08 +0800

    #24 Add /tags framework

---
+++ src/BunBlog.API/Controllers/TagController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BunBlog.API.Controllers
+{
+    [Route("api/tags")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        public TagController()
+        {
+
+        }
+        
+        [HttpGet("")]
+        public IActionResult GetList()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Post(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
