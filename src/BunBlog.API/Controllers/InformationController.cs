8d622cb7ff228ed26aa3ec8898b6ece6f98379ca
huhubun <nongzc@outlook.com>
2019-08-14 01:03:41 +0800

    Add information api.

---
+++ src/BunBlog.API/Controllers/InformationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BunBlog.API.Models.Informations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BunBlog.API.Controllers
+{
+    [Route("api/information")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        /// <summary>
+        /// 获取 Bun Blog API 程序信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("")]
+        [ProducesResponseType(typeof(InformationModel), 200)]
+        public IActionResult GetInformation()
+        {
+            Thread.Sleep(2000);
+            return Ok(new InformationModel());
+        }
+    }
+}
---
+++ src/BunBlog.API/Models/Informations/InformationModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace BunBlog.API.Models.Informations
+{
+    public class InformationModel
+    {
+        /// <summary>
+        /// 当前程序的版本号
+        /// </summary>
+        public string Version
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+    }
+}
