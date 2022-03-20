5730edc86b4ca7e8ebe5505992be35254f5594d1
huhubun <nongzc@outlook.com>
2022-03-20 23:26:45 +0800

    Information Api 新增返回字段：当前是否运行在 k8s 中。

--- src/BunBlog.Core/BunHelper.cs
+++ src/BunBlog.Core/BunHelper.cs
@@ -1,27 +1,30 @@
 ﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.IO;
 
-namespace BunBlog.Core
+namespace BunBlog.Core;
+
+public static class BunHelper
 {
-    public static class BunHelper
+    /// <summary>
+    /// 从 RuntimeInformation.OSDescription 中获取系统发行版本名称
+    /// </summary>
+    /// <param name="osDescription"><see cref="RuntimeInformation.OSDescription"/></param>
+    /// <returns>系统发行版本名称</returns>
+    public static string GetOSName(string osDescription)
     {
-        /// <summary>
-        /// 从 RuntimeInformation.OSDescription 中获取系统发行版本名称
-        /// </summary>
-        /// <param name="osDescription">RuntimeInformation.OSDescription 的返回值</param>
-        /// <returns>系统发行版本名称</returns>
-        public static string GetOSName(string osDescription)
+        if (osDescription.StartsWith("Microsoft Windows"))
         {
-            if (osDescription.StartsWith("Microsoft Windows"))
-            {
-                return "Windows";
-            }
-
-            if (new Regex("^Linux.*Debian").IsMatch(osDescription))
-            {
-                return "Debian";
-            }
-
-            return "Other";
+            return "Windows";
         }
+
+        if (new Regex("^Linux.*Debian").IsMatch(osDescription))
+        {
+            return "Debian";
+        }
+
+        return "Other";
     }
+
+    public static bool CheckKubernetesServiceAccountDirectoryExists() => Directory.Exists("/var/run/secrets/kubernetes.io/serviceaccount");
 }
